c04546b75b1fb042d7e1cdefcc946bd2945bdd18
Ian Roskow <ianroskow@gmail.com>
2019-10-13 01:19:16 +0200

    Implemented Carousel for image display

--- src/components/results.js
+++ src/components/results.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { CarouselProvider, Slider } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+import { Segment, Image } from 'semantic-ui-react';
+
+import CustomCardSlide from './CustomCardSlide';
+import CustomDotGroup from './CustomDotGroup';
 
 const results = props => {
-  const images = props.movies.map(movie => {
-    return <Image src={movie.image} rounded floated='left' />;
+  let count = 0;
+  const slides = props.movies.map(movie => {
+    count++;
+    return (
+      <CustomCardSlide
+        image={movie.image}
+        index={0}
+        header={movie.title}
+        meta={movie.type}
+      />
+    );
   });
-
-  return <Image.Group size='small'>{images}</Image.Group>;
+  //<Image src={movie.image} rounded floated='left' />
+  //<Image.Group size='small'>{images}</Image.Group>
+  return (
+    <Segment>
+      <CarouselProvider
+        naturalSlideWidth={1}
+        naturalSlideHeight={1.75}
+        totalSlides={count}
+        visibleSlides={5}
+        style={{ width: '100%' }}
+      >
+        <Slider>{slides}</Slider>
+        <CustomDotGroup slides={count} />
+      </CarouselProvider>
+    </Segment>
+  );
 };
 
 export default results;
